6660620b443c710ae90ddbc3e9a68a2b27919124
1825346742@qq.com <1825346742@qq.com>
2019-10-15 15:11:04 +0800

    委托的使用

--- DotNetCoreTest/DotNetCoreTest/Program.cs
+++ DotNetCoreTest/DotNetCoreTest/Program.cs
@@ -7,10 +7,37 @@ namespace DotNetCoreTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            dynamic obj = new System.Dynamic.ExpandoObject();
+            Program p = new Program();
+            MyDelegate d1, d2, d3;
+            d1 = new MyDelegate(p.Test1);
+            d2 = new MyDelegate(p.Test2);
+            d3 = new MyDelegate(p.Test3);
+            d1("HAHA");
+            d2("XIXI");
+            d3("xixi");
+            Test(obj);
         }
-        public void Test<T>(T parm)
-        {
 
+        delegate void MyDelegate(string name);
+        public void Test1(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public void Test2(string name)
+        {
+            Console.WriteLine(name);
+        }
+        public void Test3(string name)
+        {
+            Console.WriteLine(name);
+        }
+
+
+
+        public static void Test<T>(T parm)
+        {
+            Console.WriteLine(parm.GetType()); 
         }
 
     }
