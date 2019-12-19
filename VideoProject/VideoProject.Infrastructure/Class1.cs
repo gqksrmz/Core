88d338a7121508f43f947d3fbcd25180c443bf6a
1825346742@qq.com <1825346742@qq.com>
2019-12-19 16:33:26 +0800

    创建项目结构

---
+++ VideoProject/VideoProject.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VideoProject.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
---
+++ VideoProject/VideoProject.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VideoProject.Service
+{
+    public class Class1
+    {
+    }
+}
