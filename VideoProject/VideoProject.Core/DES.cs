ac876a1d4129c5f86360c0756a051a9da7343340
1825346742@qq.com <1825346742@qq.com>
2019-12-26 14:43:24 +0800

    DES加密解密

---
+++ VideoProject/VideoProject.Core/DES.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VideoProject.Core
+{
+    class DES
+    {
+        //加密
+        public static string DESEncrypt(string data)
+        {
+            byte[] key = new byte[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8
+            };
+            byte[] iv = new byte[]
+            {
+                8,
+                7,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1
+            };
+            byte[] byKey = key;
+            byte[] byIV = iv;
+
+            DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+            int i = cryptoProvider.KeySize;
+            MemoryStream ms = new MemoryStream();
+            CryptoStream cst = new CryptoStream(ms, cryptoProvider.CreateEncryptor(byKey, byIV), CryptoStreamMode.Write);
+
+            StreamWriter sw = new StreamWriter(cst);
+            sw.Write(data);
+            sw.Flush();
+            cst.FlushFinalBlock();
+            sw.Flush();
+            return Convert.ToBase64String(ms.GetBuffer(), 0, (int)ms.Length);
+        }
+        //解密
+        private static string DESDecrypt(byte[] byteInput)
+        {
+            byte[] key = new byte[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8
+            };
+            byte[] iv = new byte[]
+            {
+                8,
+                7,
+                6,
+                5,
+                4,
+                3,
+                2,
+                1
+            };
+            MemoryStream memoryStream = null;
+            CryptoStream cryptoStream = null;
+            StreamReader streamReader = null;
+            string text = string.Empty;
+            string result;
+            try
+            {
+                DES des = DES.Create();
+                des.Key = key;
+                des.IV = iv;
+                ICryptoTransform transform = des.CreateDecryptor();
+                memoryStream = new MemoryStream(byteInput);
+                cryptoStream = new CryptoStream(memoryStream, transform, CryptoStreamMode.Read);
+                streamReader = new StreamReader(cryptoStream);
+                text = streamReader.ReadToEnd();
+                streamReader.Close();
+                cryptoStream.Close();
+                memoryStream.Close();
+                result = text;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                if (cryptoStream != null)
+                {
+                    cryptoStream.Close();
+                }
+                if (memoryStream != null)
+                {
+                    memoryStream.Close();
+                }
+            }
+            return result;
+        }
+    }
+}
